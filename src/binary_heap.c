dec546e3c42248e25db811863015e620389c75c8
Fang Ling <alice@jxx.world>
2023-02-15 13:51:00 +0800

    add max_heapify_up

--- src/binary_heap.c
+++ src/binary_heap.c
@@ -35,4 +35,21 @@ void binary_heap_deinit(struct BinaryHeap* heap) {
 static inline Int left(Int i)   { return 2 * i + 1; }   /* Left child */
 static inline Int right(Int i)  { return 2 * i + 2; }   /* Right child */
 static inline Int parent(Int i) { return (i - 1) / 2; } /* Parent */
+
+/* Maintain the max heap property from node i all the way up to root */
+static void max_heapify_up(struct BinaryHeap* heap, Int i) {
+    if (i == 0) { /* we are at root node now */
+        return;
+    }
+    /* if parent(i).key <= i.key */
+    if (heap -> compare(array_get(heap -> storage, parent(i)),
+                        array_get(heap -> storage, i)) <= 0) {
+        /* Exchange i with parent(i) */
+        array_swap_at(heap -> storage, i, parent(i));
+        /* Current node is satisfy max heap property, but maybe not its
+         *  parent. Fix this recursively.
+         */
+        max_heapify_up(heap, parent(i));
+    }
+}
 /** End: Private helpers **/
