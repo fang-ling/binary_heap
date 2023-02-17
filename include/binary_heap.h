f74ca460e1f5b8018c2641e1883a071c294c7926
Fang Ling <alice@jxx.world>
2023-02-17 09:48:08 +0800

    add public header

--- include/binary_heap.h
+++ include/binary_heap.h
@@ -25,4 +25,33 @@ struct BinaryHeap {
     Int (*compare)(void* lhs, void* rhs);
 };
 
+/** Begin: Creating a heap **/
+/* Creates a new, empty heap. */
+struct BinaryHeap* binary_heap_init(Int element_size,
+                                    Int (*compare)(void* lhs, void* rhs));
+/** End: Creaing a heap **/
+
+/** Begin: Destroying a heap **/
+/* Destroys all the elements and deallocates all the storage capacity. */
+void binary_heap_deinit(struct BinaryHeap* heap);
+/** End: Destroying a heap **/
+
+/** Begin: Lookup **/
+/* Returns the largest element in a heap in constant time. */
+void* binary_heap_max(struct BinaryHeap* heap);
+/** End: Lookup **/
+
+/** Begin: Insertion **/
+/* Inserts a new item into heap. */
+void binary_heap_insert(struct BinaryHeap* heap, void* key);
+/** End: Insertion **/
+
+/** Begin: Removal **/
+/* Removes and Returns the max item in the heap. */
+void* binary_heap_remove(struct BinaryHeap* heap);
+
+/* Removes the max item in the heap */
+void binary_heap_removen(struct BinaryHeap *heap);
+/** End: Removal **/
+
 #endif /* binary_heap_h */
--- src/binary_heap.c
+++ src/binary_heap.c
@@ -7,10 +7,12 @@
 
 #include "binary_heap.h"
 #include "array.h"
+#include "util.h"
 
 #define var __auto_type
 
 /** Begin: Creating a heap **/
+/* Creates a new, empty heap. */
 struct BinaryHeap* binary_heap_init(Int element_size,
                                     Int (*compare)(void* lhs, void* rhs)) {
     var heap = (struct BinaryHeap*)malloc(sizeof(struct BinaryHeap));
@@ -24,6 +26,7 @@ struct BinaryHeap* binary_heap_init(Int element_size,
 /** End: Creaing a heap **/
 
 /** Begin: Destroying a heap **/
+/* Destroys all the elements and deallocates all the storage capacity. */
 void binary_heap_deinit(struct BinaryHeap* heap) {
     array_deinit(heap -> storage);
     free(heap);
@@ -31,12 +34,12 @@ void binary_heap_deinit(struct BinaryHeap* heap) {
 /** End: Destroying a heap **/
 
 /** Begin: Private helpers **/
-/* Index calculation functions */
+/* Index calculation functions. */
 static inline Int left(Int i)   { return 2 * i + 1; }   /* Left child */
 static inline Int right(Int i)  { return 2 * i + 2; }   /* Right child */
 static inline Int parent(Int i) { return (i - 1) / 2; } /* Parent */
 
-/* Maintain the max heap property from node i all the way up to root */
+/* Maintain the max heap property from node i all the way up to root. */
 static void max_heapify_up(struct BinaryHeap* heap, Int i) {
     if (i == 0) { /* we are at root node now */
         return;
@@ -53,7 +56,7 @@ static void max_heapify_up(struct BinaryHeap* heap, Int i) {
     }
 }
 
-/* Maintain the max heap property from node i all the way down to left */
+/* Maintain the max heap property from node i all the way down to leaf. */
 static void max_heapify_down(struct BinaryHeap* heap, Int i) {
     var j = 0ll;
     /* Current i has both left and right children. */
@@ -107,15 +110,31 @@ void binary_heap_insert(struct BinaryHeap* heap, void* key) {
 /** End: Insertion **/
 
 /** Begin: Removal **/
-/* Removes and Returns the max item in the heap */
+/* Removes and Returns the max item in the heap. */
 void* binary_heap_remove(struct BinaryHeap* heap) {
     /* To remove the largest element at index 0. Exchange it with the last item
      * in the array and then we trickle it down to where it belongs in the
      * heap.
      */
+    if (heap -> count == 0) {
+        fatal_error("Remove a item from a empty heap");
+    }
     heap -> count -= 1;
+    heap -> is_empty = heap -> count == 0 ? true : false;
     array_swap_at(heap -> storage, 0, heap -> count);
     max_heapify_down(heap, 0);
     return array_remove_last(heap -> storage);
+}
+
+/* Removes the max item in the heap. */
+void binary_heap_removen(struct BinaryHeap* heap) {
+    if (heap -> count == 0) {
+        fatal_error("Remove a item from a empty heap");
+    }
+    heap -> count -= 1;
+    heap -> is_empty = heap -> count == 0 ? true : false;
+    array_swap_at(heap -> storage, 0, heap -> count);
+    max_heapify_down(heap, 0);
+    array_remove_lastn(heap -> storage);
 }
 /** End: Removal **/
